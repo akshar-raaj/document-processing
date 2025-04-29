0398bd406c16c31f85cb1271dc4e34dfd9aea1bc
Akshar Raaj <raaj.akshar@gmail.com>
2025-04-29 18:31:04 +0530

    Infer the file type based on Python magic library

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install -r requirements.txt
 
 COPY *.py /app
 
-CMD ["fastapi", "dev", "main.py"]
+CMD ["fastapi", "dev", "main.py", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi import UploadFile
+
+from utils import identify_file_type
 
 
 app = FastAPI()
@@ -7,3 +10,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return "Document Processing"
+
+
+@app.post("/upload")
+async def upload(attachment: UploadFile):
+    path = f"/media/{attachment.filename}"
+    f = open(path, "wb")
+    chunk = await attachment.read(1024 * 1024)
+    while chunk:
+        f.write(chunk)
+        chunk = await attachment.read(1024 * 1024)
+    f.close()
+    # Identify the file mime type.
+    file_type = identify_file_type(path)
+    print(file_type)
+    return {"status": "processed"}
