c8595c2031f1a245cbe0564c4a691f5b4531c34c
Akshar Raaj <raaj.akshar@gmail.com>
2025-05-30 00:55:52 +0530

    Added ability to run extraction in the background. Lower latency and high scalability.

---
+++ tasks.py
@@ -0,0 +1,18 @@
+import hashlib
+from rq import Queue
+
+from db import get_connection, set_value
+
+
+def report_success(job, connection, result, *args, **kwargs):
+    is_success, extracted_text = result
+    file_path = job.args[0]
+    path_hash = hashlib.sha256(file_path.encode('utf-8')).hexdigest()
+    # Write this result to the data store.
+    set_value(path_hash, extracted_text)
+
+
+def enqueue_extraction(extraction_function, file_path):
+    connection = get_connection()
+    q = Queue(connection=connection)
+    q.enqueue(extraction_function, file_path, on_success=report_success)
