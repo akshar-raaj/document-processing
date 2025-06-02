756a9905f6c82c64dac76f18a33f0c8da80e6202
Akshar Raaj <raaj.akshar@gmail.com>
2025-06-02 21:44:27 +0530

    Missed in last commit.

--- service_wrappers.py
+++ service_wrappers.py
@@ -4,7 +4,7 @@ from image_preprocessing import preprocess_image_opencv
 from db import set_object
 
 
-def extract_image_text_and_set_db(file_path: str, key: str, field: str, options=None):
+def extract_image_text_and_set_db(file_path: str, key: str, field: str = 'content', options=None):
     if options is None:
         options = {
             "gray": True,
@@ -20,7 +20,7 @@ def extract_image_text_and_set_db(file_path: str, key: str, field: str, options=
         return False, content
 
 
-def extract_pdf_text_and_set_db(file_path: str, key: str, field: str):
+def extract_pdf_text_and_set_db(file_path: str, key: str, field: str = 'content'):
     is_success, content = extract_pdf_text_all(file_path)
     if is_success is True:
         set_object(key, field, content)
